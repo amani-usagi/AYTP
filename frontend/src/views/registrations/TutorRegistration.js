bc71cf8236b0832db9f876e10f205455e9a9a7f7
nzvki <55402864+nzvki@users.noreply.github.com>
2021-09-16 10:51:18 +0300

    Update TutorRegistration.js
    Added backend status and redirect routes

--- frontend/src/views/registrations/TutorRegistration.js
+++ frontend/src/views/registrations/TutorRegistration.js
@@ -1,8 +1,86 @@
-
-import React from "react";
-import { Link } from "react-router-dom";
+import React,{useState} from "react";
+import { Link ,useParams, useHistory } from "react-router-dom";
+import { API_URL } from "../../config";
 
 export function TutorRegistration() {
+    let title= "";
+    let history = useHistory()
+
+  const TutorRegisterrequest = (name, email, password, schoolid) => async (e) => {
+    let url = `${API_URL}/tutor/self/register`
+    e.preventDefault();
+    let query = {
+      name: name,
+      email: email,
+      password: password,
+      schoolid: schoolid
+    };
+    const headers = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(query),
+    };
+
+    alert(JSON.stringify(query) + url)
+    const response = await fetch(url,headers);
+    const data = await response.json();
+    alert(JSON.stringify(data));
+    
+    var trstatus = (data.status);
+    if (trstatus === 0 ){
+      history.push("/tutor/dashboard")
+    }
+    else{
+      history.push("/register/tutor")
+    }
+  };
+
+  let [ name,setName] = useState("");
+  let [ email,setEmail] = useState("");
+  let [password,setPassword] = useState("");
+  let [ schoolid,setSchoolid] = useState("");
+  let { type } = useParams();
+  
+  const handleNameInput = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+  };
+  
+  const handleEmailInput = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordInput = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  };
+
+  const handleSchoolidInput = (e) => {
+    e.preventDefault();
+    setSchoolid(e.target.value);
+  };
+
+  let formSubmittionHandler = () => {
+      console.log("default handler is in use")
+  };
+
+  switch (type) {
+    
+    case "tutor":
+      title ="Tutor Register"
+      formSubmittionHandler = TutorRegisterrequest(name, email, password, schoolid);
+      break;
+
+    default:
+        title ="Tutor Register"
+        formSubmittionHandler = TutorRegisterrequest(name, email, password, schoolid);
+        break;
+  }
+
+
     return (
     <>
     <div className="container mx-auto px-4 h-full">
@@ -12,6 +90,7 @@ export function TutorRegistration() {
                     <div className="rounded-t mb-0 px-6 py-6">
                         <div className="text-center mb-3">
                             <h6 className="text-gray-500 text-sm font-semibold"> Sign up with</h6>
+                            {title} with
                         </div>
                         <div className="btn-wrapper text-center">
                             <button className="bg-white active:bg-gray-50 text-gray-700 font-bold px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center text-xs ease-linear transition-all duration-150" type="button">
@@ -29,30 +108,47 @@ export function TutorRegistration() {
                         <div className="text-gray-500 text-center mb-3 font-bold">
                             <small>Or sign up with credentials</small>
                         </div>
-                        <form>
+                        <form onSubmit={formSubmittionHandler}>
                             <div className="relative w-full mb-3">
                                 <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor="grid-password">
                                     Name
                                 </label>
-                                <input type="email" className="border-0 px-3 py-3 placeholder-gray-300 text-gray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Name"/>
+                                <input 
+                                type="text"
+                                onChange={handleNameInput}
+                                className="border-0 px-3 py-3 placeholder-gray-300 text-gray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                                placeholder="Name"/>
                             </div>
                             <div className="relative w-full mb-3">
                                 <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor="grid-password">
                                     Email
                                 </label>
-                                <input type="email" className="border-0 px-3 py-3 placeholder-gray-300 text-gray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"placeholder="Email"/>
+                                <input 
+                                type="email"
+                                onChange={handleEmailInput}
+                                className="border-0 px-3 py-3 placeholder-gray-300 text-gray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                                placeholder="Email"
+                                />
                             </div>
                             <div className="relative w-full mb-3">
                                 <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor="grid-password">
                                     Password
                                 </label>
-                                <input type="password" className="border-0 px-3 py-3 placeholder-gray-300 text-gray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Password"/>
+                                <input 
+                                type="password"
+                                onChange={handlePasswordInput}
+                                className="border-0 px-3 py-3 placeholder-gray-300 text-gray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                                placeholder="Password"/>
                             </div>
                             <div className="relative w-full mb-3">
                                 <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor="grid-password">
-                                    Confirm Password
+                                    School ID
                                 </label>
-                                <input type="password" className="border-0 px-3 py-3 placeholder-gray-300 text-gray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Confirm Password"/>
+                                <input
+                                type="number"
+                                onChange={handleSchoolidInput}
+                                className="border-0 px-3 py-3 placeholder-gray-300 text-gray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                                placeholder="School ID"/>
                             </div>
                             <div>
                                 <label className="inline-flex items-center cursor-pointer">
@@ -64,7 +160,9 @@ export function TutorRegistration() {
                                 </label>
                             </div>
                             <div className="text-center mt-6">
-                                <button className="bg-blue-500 hover:bg-blue-900 text-white active:bg-gray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150" type="button">
+                                <button 
+                                onClick={formSubmittionHandler}
+                                className="bg-blue-500 hover:bg-blue-900 text-white active:bg-gray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150" type="button">
                                     Create Account
                                 </button>
                             </div>
