bebe5edc46e1b703c4a45a239ac93133a00a2fb7
nzvki <55402864+nzvki@users.noreply.github.com>
2021-10-28 21:57:37 +0300

    Update Schools.js
    Tutor create new course function

--- frontend/src/components/Cards/Schools.js
+++ frontend/src/components/Cards/Schools.js
@@ -1,160 +1,178 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { API_URL } from "../../config";
+import { getUserToken } from "../../services/authenticationService";
 
-// components
+export default function CreateCourse() {
+  //let title = ""
+  let history = useHistory();
+  const TutorCourseCreate = (name, price, description, key) => async (e) => {
+    let url = `${API_URL}/tutor/create/course`;
+    e.preventDefault();
+    let query = {
+      name: name,
+      price: price,
+      description: description,
+      key: key,
+    };
+    const headers = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Token: getUserToken(),
+      },
+      body: JSON.stringify(query),
+    };
 
-import TableDropdown from "../Dropdowns/TableDropdown";
+    alert(JSON.stringify(query) + url);
+    const response = await fetch(url, headers);
+    const data = await response.json();
+    alert(JSON.stringify(data));
 
-export default function Schools({ color }) {
-	return (
+    var arstatus = data.status;
+    if (arstatus === 0) {
+      history.push("/tutor/dashboard");
+    } else {
+      history.push("/tutor/course");
+    }
+  };
+
+  let [name, setName] = useState("");
+  let [price, setPrice] = useState("");
+  let [description, setDescription] = useState("");
+  let [key, setKey] = useState("");
+  let { type } = useParams();
+  const handleNameInput = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+  };
+
+  const handlePriceInput = (e) => {
+    e.preventDefault();
+    setPrice(e.target.value);
+  };
+
+  const handleDescriptionInput = (e) => {
+    e.preventDefault();
+    setDescription(e.target.value);
+  };
+
+  const handleKeyInput = (e) => {
+    e.preventDefault();
+    setKey(e.target.value);
+  };
+
+  let formSubmittionHandler = () => {
+    console.log("default handler is in use");
+  };
+
+  switch (type) {
+    case "tutor":
+      //title = "Create Course";
+      formSubmittionHandler = TutorCourseCreate(name, price, description, key);
+      break;
+
+    default:
+      //title = "Create Course";
+      formSubmittionHandler = TutorCourseCreate(name, price, description, key);
+      break;
+  }
+  return (
     <>
-    <div className={ "relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded " + (color === "light" ? "bg-white" : "bg-blue-900 text-white") } >
-        <div className="rounded-t mb-0 px-4 py-3 border-0">
-          	<div className="flex flex-wrap items-center">
-            	<div className="relative w-full px-4 max-w-full flex-grow flex-1">
-					<h3 className={ "font-semibold text-lg " + (color === "light" ? "text-gray-700" : "text-white") } >
-						Schools
-					</h3>
-            	</div>
-        	</div>
-    	</div>
-		<div className="block w-full overflow-x-auto">
-			<table className="items-center w-full bg-transparent border-collapse">
-				<thead>
-					<tr>
-						<th className={ "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " + (color === "light" ? "bg-gray-50 text-gray-500 border-gray-100" : "bg-blue-800 text-blue-300 border-blue-700") } >
-							School Name
-						</th>
-						<th className={ "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " + (color === "light" ? "bg-gray-50 text-gray-500 border-gray-100" : "bg-blue-800 text-blue-300 border-blue-700") } >
-							Billing
-						</th>
-						<th className={ "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " + (color === "light" ? "bg-gray-50 text-gray-500 border-gray-100" : "bg-blue-800 text-blue-300 border-blue-700") } >
-							Status
-						</th>
-						<th className={ "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " + (color === "light" ? "bg-gray-50 text-gray-500 border-gray-100" : "bg-blue-800 text-blue-300 border-blue-700") } >
-							Owner(s)
-						</th>
-						<th className={ "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " + (color === "light" ? "bg-gray-50 text-gray-500 border-gray-100" : "bg-blue-800 text-blue-300 border-blue-700") } >
-							Traffic Per Day
-						</th>
-						<th className={ "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " + (color === "light" ? "bg-gray-50 text-gray-500 border-gray-100" : "bg-blue-800 text-blue-300 border-blue-700") } >
-							Actions
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
-							<img src={require("../../assets/img/bprofile.svg").default} className="h-12 w-12 bg-white rounded-full border" alt="..." ></img>{" "}
-							<span className={ "ml-3 font-bold " + + (color === "light" ? "text-gray-600" : "text-white") } >
-									Argon Design School
-							</span>
-						</th>
-						<td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-							$2,500 USD
-						</td>
-						<td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-							<i className="fas fa-circle text-red-500 mr-2"></i> Pending
-						</td>
-						<td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-							<div className="flex">
-								<img src={require("../../assets/img/bprofile.svg").default} alt="..." className="w-10 h-10 rounded-full border-2 border-gray-50 shadow"></img>
-								<img src={require("../../assets/img/bprofile.svg").default} alt="..." className="w-10 h-10 rounded-full border-2 border-gray-50 shadow"></img>
-								<img src={require("../../assets/img/bprofile.svg").default} alt="..." className="w-10 h-10 rounded-full border-2 border-gray-50 shadow"></img>
-							</div>
-						</td>
-						<td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-							<div className="flex items-center">
-								<span className="mr-2">60%</span>
-								<div className="relative w-full">
-									<div className="overflow-hidden h-2 text-xs flex rounded bg-yellow-200">
-										<div style={{ width: "60%" }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-yellow-500" ></div>
-									</div>
-								</div>
-							</div>
-						</td>
-						<td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
-							<TableDropdown />
-						</td>
-					</tr>
-					<tr>
-						<th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
-							<img src={require("../../assets/img/bprofile.svg").default} className="h-12 w-12 bg-white rounded-full border" alt="..." ></img>{" "}
-							<span className={ "ml-3 font-bold " + + (color === "light" ? "text-gray-600" : "text-white") } >
-								Angular Design School
-							</span>
-						</th>
-						<td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-							$1,500 USD
-						</td>
-						<td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-							<i className="fas fa-circle text-green-500 mr-2"></i> Completed
-						</td>
-						<td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-							<div className="flex">
-								<img src={require("../../assets/img/bprofile.svg").default} alt="..." className="w-10 h-10 rounded-full border-2 border-gray-50 shadow"></img>
-							</div>
-						</td>
-						<td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-							<div className="flex items-center">
-								<span className="mr-2">80%</span>
-								<div className="relative w-full">
-									<div className="overflow-hidden h-2 text-xs flex rounded bg-green-200">
-										<div style={{ width: "80%" }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500" ></div>
-									</div>
-								</div>
-							</div>
-						</td>
-						<td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
-							<TableDropdown />
-						</td>
-					</tr>
-					<tr>
-						<th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
-							<img src={require("../../assets/img/bprofile.svg").default} className="h-12 w-12 bg-white rounded-full border" alt="..." ></img>{" "}
-							<span className={ "ml-3 font-bold " + + (color === "light" ? "text-gray-600" : "text-white") } >
-								React Design School
-							</span>
-						</th>
-						<td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-							$3,500 USD
-						</td>
-						<td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-							<i className="fas fa-circle text-red-500 mr-2"></i> pending
-						</td>
-						<td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-							<div className="flex">
-								<img src={require("../../assets/img/bprofile.svg").default} alt="..." className="w-10 h-10 rounded-full border-2 border-gray-50 shadow"></img>
-								<img src={require("../../assets/img/bprofile.svg").default} alt="..." className="w-10 h-10 rounded-full border-2 border-gray-50 shadow"></img>
-								<img src={require("../../assets/img/bprofile.svg").default} alt="..." className="w-10 h-10 rounded-full border-2 border-gray-50 shadow"></img>
-							</div>
-						</td>
-						<td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-							<div className="flex items-center">
-								<span className="mr-2">60%</span>
-								<div className="relative w-full">
-									<div className="overflow-hidden h-2 text-xs flex rounded bg-red-200">
-										<div style={{ width: "60%" }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500" ></div>
-									</div>
-								</div>
-							</div>
-						</td>
-						<td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
-							<TableDropdown />
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-    </div>
+      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-50 border-0">
+        <div className="rounded-t bg-white mb-0 px-6 py-6">
+          <div className="text-center flex justify-between">
+            <h6 className="text-gray-700 text-xl font-bold">
+              Create New Course
+            </h6>
+          </div>
+        </div>
+        <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+          <form onSubmit={formSubmittionHandler}>
+            <div className="flex flex-wrap">
+              <div className="w-full lg:w-6/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-gray-500 text-xs font-bold mb-2"
+                    htmlFor="grid-password">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Course name"
+                    onChange={handleNameInput}
+                    className="border-0 px-3 py-3 placeholder-gray-300 text-gray-500 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-6/12 px-4"></div>
+              <div className="w-full lg:w-6/12 px-4"></div>
+              <div className="w-full lg:w-6/12 px-4"></div>
+            </div>
+            <hr className="mt-6 border-b-1 border-gray-300" />
+            <div className="flex flex-wrap">
+              <div className="w-full lg:w-12/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-gray-500 text-xs font-bold mb-2"
+                    htmlFor="grid-password">
+                    Price
+                  </label>
+                  <input
+                    type="number"
+                    placeholder="Course Price"
+                    onChange={handlePriceInput}
+                    className="border-0 px-3 py-3 placeholder-gray-300 text-gray-500 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-4/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-gray-500 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    Key
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Course Key"
+                    onChange={handleKeyInput}
+                    className="border-0 px-3 py-3 placeholder-gray-300 text-gray-500 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-4/12 px-4"></div>
+              <div className="w-full lg:w-4/12 px-4"></div>
+            </div>
+            <hr className="mt-6 border-b-1 border-gray-300" />
+            <h6 className="text-gray-500 text-sm mt-3 mb-6 font-bold uppercase">
+              Description
+            </h6>
+            <div className="flex flex-wrap">
+              <div className="w-full lg:w-12/12 px-4">
+                <div className="relative w-full mb-3">
+                  <input
+                    type="text"
+                    placeholder="Course descriptio"
+                    onChange={handleDescriptionInput}
+                    className="border-0 px-3 py-3 placeholder-gray-300 text-gray-500 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    rows="5"
+                  ></input>
+
+                  <button
+                    className="bg-blue-500 text-white active:bg-blue-500 font-bold text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={formSubmittionHandler}
+                  >
+                    <i className="fa fa-plus mr-2" aria-hidden="true"></i>Create
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
-	);
+  );
 }
-
-Schools.defaultProps = {
-  color: "light",
-};
-
-Schools.propTypes = {
-  color: PropTypes.oneOf(["light", "dark"]),
-};
